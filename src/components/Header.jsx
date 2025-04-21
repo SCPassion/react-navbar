566623c91ddb8bd37844e1a7248a0126f38c60f2
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-04-21 21:45:38 +0200

    Add react-icons dependency and implement Header component with a hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,27 @@
 import { Outlet } from "react-router"
+import { useState } from "react"
+import { GiHamburgerMenu } from "react-icons/gi"
+
 export default function Header() {
+  const [open, setOpen] = useState(false)
+
+  function toggle() {
+    setOpen((prev) => !prev)
+  }
+
   return (
     <div>
-      <h1>UppLayer</h1>
+      <nav className="flex items-center justify-between bg-gray-800 p-4 text-xl text-white">
+        <h1>MySite</h1>
+
+        {/* A Hamburger button for small screen */}
+        <button
+          className="cursor-pointer text-xl hover:rotate-90 md:hidden"
+          onClick={toggle}
+        >
+          <GiHamburgerMenu size={24} />
+        </button>
+      </nav>
       <Outlet />
     </div>
   )
